66d8e81b68768d3d77352162ec89e08a88a440f7
acroisie <acroisie@student.42lyon.fr>
2022-12-21 18:40:38 +0100

    fix segfault TOPIC

--- srcs/commands/Topic.cpp
+++ srcs/commands/Topic.cpp
@@ -7,35 +7,40 @@ void	Server::topic(Client &client)
 
 	if (segvGuard(client))
 		return ;
-	if (!_channelMap[client.getTokens()[1]]->isOp(client))
+	if (_channelMap.count(client.getTokens()[1]))
 	{
-		client.setReply(ERR_CHANOPRIVSNEEDED(client.getNickname(), client.getTokens()[1]));
-		return;
+		if (!_channelMap[client.getTokens()[1]]->isOp(client))
+		{
+			client.setReply(ERR_CHANOPRIVSNEEDED(client.getNickname(), client.getTokens()[1]));
+			return;
+		}
+		if (!_channelMap.count(client.getTokens()[1]))
+		{
+			client.setReply(ERR_NOSUCHCHANNEL(client.getNickname(), ));
+			return ;
+		}
+		if (!_channelMap[client.getTokens()[1]]->clientIsOnChan(client))
+		{
+			client.setReply(ERR_NOTONCHANNEL(client.getNickname(), client.getTokens()[1]));
+			return ;
+		}
+		if (client.getTokens().size() == 3)
+		{
+			topic =  client.getTokens()[2].substr(1,  client.getTokens()[2].size());
+			_channelMap[client.getTokens()[1]]->setTopic(topic);
+			if (topic.compare(":") == 0)
+				_channelMap[client.getTokens()[1]]->setTopic("");
+			client.setReply(RPL_TOPICC(client.getNickname(), client.getTokens()[1], _channelMap[client.getTokens()[1]]->getTopic()));
+			notice(client,client.getTokens()[1], RPL_TOPICC(client.getNickname(), client.getTokens()[1], _channelMap[client.getTokens()[1]]->getTopic()));
+		}
+		else if (client.getTokens().size() == 2)
+		{
+			if (_channelMap[client.getTokens()[1]]->getTopic().empty())
+				client.setReply(RPL_NOTOPIC(client.getNickname(), client.getTokens()[1]));
+			else
+				client.setReply(RPL_TOPIC(client.getNickname(), client.getTokens()[1], _channelMap[client.getTokens()[1]]->getTopic()));
+		}
 	}
-	if (!_channelMap.count(client.getTokens()[1]))
-	{
-		client.setReply(ERR_NOSUCHCHANNEL(client.getNickname(), ));
-		return ;
-	}
-	if (!_channelMap[client.getTokens()[1]]->clientIsOnChan(client))
-	{
-		client.setReply(ERR_NOTONCHANNEL(client.getNickname(), client.getTokens()[1]));
-		return ;
-	}
-	if (client.getTokens().size() == 3)
-	{
-		topic =  client.getTokens()[2].substr(1,  client.getTokens()[2].size());
-		_channelMap[client.getTokens()[1]]->setTopic(topic);
-		if (topic.compare(":") == 0)
-			_channelMap[client.getTokens()[1]]->setTopic("");
-		client.setReply(RPL_TOPICC(client.getNickname(), client.getTokens()[1], _channelMap[client.getTokens()[1]]->getTopic()));
-		notice(client,client.getTokens()[1], RPL_TOPICC(client.getNickname(), client.getTokens()[1], _channelMap[client.getTokens()[1]]->getTopic()));
-	}
-	else if (client.getTokens().size() == 2)
-	{
-		if (_channelMap[client.getTokens()[1]]->getTopic().empty())
-			client.setReply(RPL_NOTOPIC(client.getNickname(), client.getTokens()[1]));
-		else
-			client.setReply(RPL_TOPIC(client.getNickname(), client.getTokens()[1], _channelMap[client.getTokens()[1]]->getTopic()));
-	}
+	else 
+		client.setReply(ERR_NOSUCHNICK(client.getNickname(), client.getTokens()[1]));
 }
