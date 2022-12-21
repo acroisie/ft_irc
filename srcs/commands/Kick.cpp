fa7dd25744fdbb941d8b81a1a64965b77939efbe
acroisie <acroisie@student.42lyon.fr>
2022-12-21 13:54:58 +0100

    update KICK

--- srcs/commands/Kick.cpp
+++ srcs/commands/Kick.cpp
@@ -5,33 +5,38 @@ void	Server::kick(Client &client)
 {
 	if (segvGuard(client))
 		return ;
-	if (_channelMap[client.getTokens()[1]]->isOp(client))
-	{	
-		string	comment;
-		vector<string>::iterator it = client.getTokens().begin();
-		if (client.getTokens().size() > 3)
-		{
-			it += 3;
-			while (true)
+	if (_channelMap.count(client.getTokens()[1]))
+	{
+		if (_channelMap[client.getTokens()[1]]->isOp(client))
+		{	
+			string	comment;
+			vector<string>::iterator it = client.getTokens().begin();
+			if (client.getTokens().size() > 3)
 			{
-				comment += *it;
-				it++;
-				if (it == client.getTokens().end())
-					break;
-				comment += " ";
+				it += 3;
+				while (true)
+				{
+					comment += *it;
+					it++;
+					if (it == client.getTokens().end())
+						break;
+					comment += " ";
+				}
+			}
+			int fd = _channelMap[client.getTokens()[1]]->getNameFd(client.getTokens()[2]); ;
+			if (!fd)
+				client.setReply(ERR_NOSUCHNICK(client.getNickname(), client.getTokens()[1]));
+			else if (_channelMap[client.getTokens()[1]]->isOp(client))
+			{
+				client.setReply(RPL_KICK(client.getNickname(), _clientMap[fd].getNickname(), client.getTokens()[1], (comment.empty() ? "" : comment)));
+				notice(client, client.getTokens()[1], RPL_KICK(client.getNickname(), _clientMap[fd].getNickname(), client.getTokens()[1], (comment.empty() ? "" : comment)));
+				_channelMap[client.getTokens()[1]]->eraseClient(_clientMap[fd]);
 			}
 		}
-		int fd = _channelMap[client.getTokens()[1]]->getNameFd(client.getTokens()[2]); ;
-		if (!fd)
-			client.setReply(ERR_NOSUCHNICK(client.getNickname(), client.getTokens()[1]));
-		else if (_channelMap[client.getTokens()[1]]->isOp(client))
-		{
-			client.setReply(RPL_KICK(client.getNickname(), _clientMap[fd].getNickname(), client.getTokens()[1], (comment.empty() ? "" : comment)));
-			notice(client, client.getTokens()[1], RPL_KICK(client.getNickname(), _clientMap[fd].getNickname(), client.getTokens()[1], (comment.empty() ? "" : comment)));
-			_channelMap[client.getTokens()[1]]->eraseClient(_clientMap[fd]);
-		}
+		else
+			client.setReply(ERR_CHANOPRIVSNEEDED(client.getNickname(), client.getTokens()[1]));
 	}
-	else
-		client.setReply(ERR_CHANOPRIVSNEEDED(client.getNickname(), client.getTokens()[1]));
+	else 
+		client.setReply(ERR_NOSUCHNICK(client.getNickname(), client.getTokens()[1]));
 }
 
