695545649e091a762adf2b6f87996d1d47840a8f
acroisie <acroisie@student.42lyon.fr>
2022-12-21 13:22:14 +0100

    update INVITE

--- srcs/commands/Invite.cpp
+++ srcs/commands/Invite.cpp
@@ -5,23 +5,28 @@ void	Server::invite(Client &client)
 {
 	if (segvGuard(client))
 		return ;
-	if (_channelMap[client.getTokens()[1]]->isOp(client))
+	if (_channelMap.count(client.getTokens()[1]))
 	{
-		map<int, Client>::iterator it = _clientMap.begin();
-		while (it != _clientMap.end())
+		if (_channelMap[client.getTokens()[1]]->isOp(client))
 		{
-			if (it->second.getNickname().compare(client.getTokens()[1]) == 0)
+			map<int, Client>::iterator it = _clientMap.begin();
+			while (it != _clientMap.end())
 			{
-				it->second.setReply(RPL_INVITING(client.getNickname(), client.getTokens()[1], client.getTokens()[2]));
-				FD_SET(it->second.getFd(), &_writeFds);
-				_channelMap[client.getTokens()[2]]->setInvited(client.getTokens()[1]);
-				break;
+				if (it->second.getNickname().compare(client.getTokens()[1]) == 0)
+				{
+					it->second.setReply(RPL_INVITING(client.getNickname(), client.getTokens()[1], client.getTokens()[2]));
+					FD_SET(it->second.getFd(), &_writeFds);
+					_channelMap[client.getTokens()[2]]->setInvited(client.getTokens()[1]);
+					break;
+				}
+				it++;
 			}
-			it++;
+			if (it == _clientMap.end())
+				client.setReply(ERR_NOSUCHNICK(client.getNickname(), client.getTokens()[1]));
 		}
-		if (it == _clientMap.end())
-			client.setReply(ERR_NOSUCHNICK(client.getNickname(), client.getTokens()[1]));
+		else
+			client.setReply(ERR_CHANOPRIVSNEEDED(client.getNickname(), client.getTokens()[1]));
 	}
-	else
+	else 
 		client.setReply(ERR_NOSUCHNICK(client.getNickname(), client.getTokens()[1]));
 }
